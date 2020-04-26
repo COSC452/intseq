59366a19c96a928ebb45840baa48647e2a82cc7b
mcgirjau <mgirjau21@amherst.edu>
2020-04-26 17:51:02 -0400

    Remove redundant stuff

---
+++ README.md
@@ -0,0 +1,34 @@
+# intseq
+
+A Clojure library using genetic programming techniques to find formulas for given sequences.
+
+
+## Motivation
+After browsing the [On-Line Encyclopedia of Integer Sequences (OEIS)](https://oeis.org/), we noticed that many of the sequences listed in the database were without formulas. Given the nature of Genetic Programming (GP) to discover solutions to problems that humans do not know how to solve directly, we knew there was an opportunity to create a program that takes advantage of GP in order to solve the problem of finding a formula. IntSeq attempts to find a formula that holds true for the first n integers in the sequence. Our program also includes testing the formulas for accuracy by seeing whether they hold true for numbers n+1 and beyond. 
+
+## Screenshots
+
+## Tech Used
+
+## Features
+
+## Code Example
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2020 FIXME
+
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
+
+This Source Code may also be made available under the following Secondary
+Licenses when the conditions for such availability set forth in the Eclipse
+Public License, v. 2.0 are satisfied: GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or (at your
+option) any later version, with the GNU Classpath Exception which is available
+at https://www.gnu.org/software/classpath/license.html.
---
+++ src/intseq/ops.clj
@@ -0,0 +1,138 @@
+(ns intseq.ops
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.math.combinatorics :as combo]))
+
+(defn add [stack]
+  (if (< (count stack) 2)
+    stack
+    (try (cons (+ (second stack) (first stack))
+               (rest (rest stack)))
+         (catch ArithmeticException e '(:overflow)))))
+
+(defn sub [stack]
+  (if (< (count stack) 2)
+    stack
+    (try (cons (- (second stack) (first stack))
+               (rest (rest stack)))
+         (catch ArithmeticException e '(:overflow)))))
+
+(defn mult [stack]
+  (if (< (count stack) 2)
+    stack
+    (try (cons (* (second stack) (first stack))
+               (rest (rest stack)))
+         (catch ArithmeticException e '(:overflow)))))
+
+(defn div [stack]
+  (if (or (< (count stack) 2)
+          (zero? (first stack)))
+    stack
+    (try (cons (long (/ (second stack) (first stack)))
+               (rest (rest stack)))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn mod- [stack]
+  (if (or (< (count stack) 2)
+          (zero? (first stack)))
+    stack
+    (cons (mod (second stack) (first stack))
+          (rest (rest stack)))))
+
+(defn expt [stack]
+  (if (or (< (count stack) 2)
+          (neg? (first stack)))
+    stack
+    (try (cons (long (math/expt (second stack) (first stack)))
+               (rest (rest stack)))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn max- [stack]
+  "Pushes the maximum of the top two items onto the top of the stack."
+  (if (< (count stack) 2)
+    stack
+    (cons (max (second stack) (first stack))
+          (rest (rest stack)))))
+
+(defn min- [stack]
+  "Pushes the minimum of the top two items onto the top of the stack."
+  (if (< (count stack) 2)
+    stack
+    (cons (min (second stack) (first stack))
+          (rest (rest stack)))))
+
+(defn log_e [stack]
+  (if (< (count stack) 1)
+    stack
+    (try (cons (long (Math/log (math/abs (first stack))))
+               (rest stack))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn log_10 [stack]
+  (if (< (count stack) 1)
+    stack
+    (try (cons (long (Math/log10 (math/abs (first stack))))
+               (rest stack))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn abs [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (math/abs (first stack))
+          (rest stack))))
+
+(defn gcd [stack]
+  (if (< (count stack) 2)
+    stack
+    (cons (math/gcd (second stack) (first stack))
+          (rest (rest stack)))))
+
+(defn lcm [stack]
+  (if (< (count stack) 2)
+    stack
+    (try (cons (long (math/lcm (second stack) (first stack)))
+               (rest (rest stack)))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn sqrt [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (long (Math/sqrt (first stack)))
+          (rest stack))))
+
+(defn cbrt [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (long (Math/cbrt (first stack)))
+          (rest stack))))
+
+(defn sin [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (long (Math/sin (first stack)))
+          (rest stack))))
+
+(defn cos [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (long (Math/cos (first stack)))
+          (rest stack))))
+
+(defn tan [stack]
+  (if (< (count stack) 1)
+    stack
+    (cons (long (Math/tan (first stack)))
+          (rest stack))))
+
+(defn perm [stack]
+  (if (< (count stack) 1)
+    stack
+    (try (cons (long (combo/count-permutations (range (first stack))))
+               (rest stack))
+         (catch IllegalArgumentException e '(:overflow)))))
+
+(defn comb [stack]
+  (if (< (count stack) 2)
+    stack
+    (try (cons (long (combo/count-combinations (range (second stack)) (first stack)))
+               (rest (rest stack)))
+         (catch IllegalArgumentException e '(:overflow)))))

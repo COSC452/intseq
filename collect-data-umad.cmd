9bab287c177b26e98f3cd5321f9b10dd9260a3c3
mcgirjau <mgirjau21@amherst.edu>
2020-04-29 14:10:35 -0400

    Add data

--- collect-data-umad.cmd
+++ collect-data-umad.cmd
@@ -5,25 +5,6 @@ initialdir   = /mnt/scratch/mgirjau21/intseq
 getenv       = True
 executable   = run
 
-:: This file only runs two main cases of interest - UMAD without crossover, and UMAD with single-point crossover
-::
-:: All other parameters are set at commonly-accepted "optimal" values used in research, and based on Prof. Spector's
-:: paper on "Program Synthesis using Uniform Mutation by Addition and Deletion"
-::
-:: USING: population-size = 1000
-::        generations = 300
-::        seq-id = (:simple, :A037270, :A000292, :A114241, :A168392)
-::        selection-type = :lexicase-selection
-::        tournament-size = 0 (IGNORED)
-::     *  crossover? = true / false
-::     *  crossover-type :single-point-crossover (IGNORED when crossover? is false)
-::        mutate? = true
-::        umad-add-rate = 0.09
-::        umad-del-rate = 0.1 (shrinking UMAD exhibits least variability)
-::        elitism? = true
-::        report? = false
-::        export-stats? = true
-
 ## Job properties
 arguments = 1000 300 :simple :lexicase-selection 0 false :single-point-crossover true 0.09 0.1 true false true
 queue 100
